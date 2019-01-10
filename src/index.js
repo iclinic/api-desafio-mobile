69b01805815e2af708564fd2a740637c1175def9
Paulo Rezende <rezendepauloeduardo@outlook.com>
2019-01-10 17:10:44 -0200

    feat: add end point to register new users

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const jwt = require('jsonwebtoken');
 
 // Files
 const middlewareLogin = require('./middlewares/login');
+const middlewareRegister = require('./middlewares/register');
 
 // Variables
 const app = express();
@@ -61,6 +62,40 @@ app.post('/auth/login', middlewareLogin, (req, res) => {
       expiresIn: 86400
     }
   );
+
+  res.json({ auth: true, token });
+});
+
+app.post('/auth/register', middlewareRegister, (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(406).json({ error: errors.array() });
+  }
+
+  const fileData = JSON.parse(fs.readFileSync(databaseFileName));
+
+  const { name, email, password } = req.body;
+
+  const userInfo = fileData.filter((user) => user.email === email);
+
+  if (userInfo.length !== 0) {
+    return res.status(406).json({ error: 'Usuário já cadastrado' });
+  }
+
+  const newUser = {
+    name,
+    email,
+    password
+  };
+
+  fileData.push(newUser);
+
+  fs.writeFileSync(databaseFileName, JSON.stringify(fileData, null, 2));
+
+  const token = jwt.sign({ name, email }, '49084b52d739f28aaaba047393d54623', {
+    expiresIn: 86400
+  });
 
   res.json({ auth: true, token });
 });
