38dddb3d02156016e7129176f602c37044cd21c8
Paulo Rezende <rezendepauloeduardo@outlook.com>
2019-01-10 15:39:35 -0200

    chore: add comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+// Packages
 const fs = require('fs');
 const express = require('express');
 
+// Variables
 const app = express();
 const port = 3000;
 const databaseFileName = 'db.json';
 
+/**
+ * Verify if the file to store the informations exists,
+ * if not, create a empty file
+ */
 fs.access(databaseFileName, fs.constants.F_OK, (err) => {
   if (err) {
     fs.writeFileSync(databaseFileName, '{}', (err) => {
@@ -17,8 +23,10 @@ fs.access(databaseFileName, fs.constants.F_OK, (err) => {
   }
 });
 
+// Routes
 app.get('/', (req, res) => res.send('Welcome'));
 
+// Start the server
 app.listen(port, () => {
   console.log(`Servidor rodando em 'https://localhost:${port}'`);
 });
