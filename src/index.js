e5cb91c77406b0dcb82f50258f839ac8daa67884
Paulo Rezende <rezendepauloeduardo@outlook.com>
2019-01-10 15:36:34 -0200

    feat: validate if the database file exists

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 const port = 3000;
+const databaseFileName = 'db.json';
+
+fs.access(databaseFileName, fs.constants.F_OK, (err) => {
+  if (err) {
+    fs.writeFileSync(databaseFileName, '{}', (err) => {
+      if (err) {
+        throw new Error(
+          'Não foi possivel criar o arquivo para armazenar as informações'
+        );
+      }
+    });
+  }
+});
 
 app.get('/', (req, res) => res.send('Welcome'));
 
