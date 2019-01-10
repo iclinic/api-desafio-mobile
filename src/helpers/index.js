318ecf3357487ba44de1a82a27c254c8f08e6368
Paulo Rezende <rezendepauloeduardo@outlook.com>
2019-01-10 17:38:09 -0200

    refactor: improve the creation of databases

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,16 +1,20 @@
 // Packages
 const fs = require('fs');
 
-const createFile = function(dbName) {
-  fs.writeFileSync(dbName, '[]', (err) => {
+const createDatabase = function(dbName) {
+  fs.access(dbName, fs.constants.F_OK, (err) => {
     if (err) {
-      throw new Error(
-        `Não foi possivel criar o arquivo ${dbName} para armazenar as informações`
-      );
+      fs.writeFileSync(dbName, '[]', (err) => {
+        if (err) {
+          throw new Error(
+            `Não foi possivel criar o arquivo ${dbName} para armazenar as informações`
+          );
+        }
+      });
     }
   });
 };
 
 module.exports = {
-  createFile
+  createDatabase
 };
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 // Files
 const controllerAuth = require('./controllers/auth');
-const { createFile } = require('./helpers');
+const { createDatabase } = require('./helpers');
 
 // Variables
 const app = express();
@@ -20,17 +20,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
  * Verify if the file to store the informations exists,
  * if not, create a empty file
  */
-fs.access(databaseFileUsers, fs.constants.F_OK, (err) => {
-  if (err) {
-    createFile(databaseFileUsers);
-  }
-});
-
-fs.access(databaseFileUsers, fs.constants.F_OK, (err) => {
-  if (err) {
-    createFile(databaseFilePosts);
-  }
-});
+createDatabase(databaseFileUsers);
+createDatabase(databaseFilePosts);
 
 // Routes
 app.get('/', (req, res) => res.send('Welcome'));
