401e9db57fd738d5d994ba6269911b558702b3cb
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2019-01-21 16:27:18 -0200

    feat: enable CORS

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const { createDatabase } = require('./src/helpers');
 // Variables
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
